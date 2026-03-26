dc7c7f3da539c118233f89293902cdcb2f782dd9
ezrahojc <ezrahojc@outlook.com>
2026-03-26 14:33:00 +0800

    fixed errors

--- app/src/main/java/com/example/teacherapp/navigation/auth/StartScreen.kt
+++ app/src/main/java/com/example/teacherapp/navigation/auth/StartScreen.kt
@@ -1,6 +1,7 @@
 package com.example.teacherapp
 
 import android.Manifest
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
